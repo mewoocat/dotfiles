18d8dbb026f4b218558c193a3aa9c8dd749bd9df
mewoocat <azure.horizon.4@gmail.com>
2022-09-06 16:32:26 -0500

    Fixed wifi disconnected icon

--- .config/awesome/wibar.lua
+++ .config/awesome/wibar.lua
@@ -1,4 +1,3 @@
-
 local awful = require("awful")
 local wibox = require("wibox")
 local gears = require("gears")
@@ -23,177 +22,187 @@ local launcher = require("widgets/launcher")
 
 
 awful.screen.connect_for_each_screen(function(s)
-    -- Wallpaper
-    set_wallpaper(s)
+  -- Wallpaper
+  set_wallpaper(s)
 
-    -- Each screen has its own tag table.
-	awful.tag({" 1 ", " 2 ", " 3 ", " 4 ", "5"  }, s, awful.layout.layouts[1])
+  -- Each screen has its own tag table.
+  awful.tag({ " 1 ", " 2 ", " 3 ", " 4 ", "5" }, s, awful.layout.layouts[1])
 
-	
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
-    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
-	s.mylayoutbox = wibox.container.margin(
-		s.mylayoutbox,
-		6, 6, 6, 6
-	)
-    s.mylayoutbox:buttons(gears.table.join(
-                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
-                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
-    -- Create a taglist widget
 
-	s.mytaglist = awful.widget.taglist {
-    		screen  = s,
-    		filter  = awful.widget.taglist.filter.all,
-    		layout   = {
-    		    spacing = 0,
-    		    layout  = wibox.layout.fixed.horizontal
-    		},
-			buttons = {
-            awful.button({ }, 1, function(t) t:view_only() end),
-            awful.button({ modkey }, 1, function(t)
-                                            if client.focus then
-                                                client.focus:move_to_tag(t)
-                                            end
-										end),
-			awful.button({ }, 3, awful.tag.viewtoggle),
-			awful.button({ modkey }, 3, function(t)
-                                            if client.focus then
-                                                client.focus:toggle_tag(t)
-                                            end
-                                        end),
-            awful.button({ }, 4, function(t) awful.tag.viewprev(t.screen) end),
-            awful.button({ }, 5, function(t) awful.tag.viewnext(t.screen) end),
-        	},
-    		widget_template = {
-    		    {
-						{
-							{
-								{
-									id     = "index_role",
-									opacity = 0,
-									widget = wibox.widget.textbox,
-								},
-								bg     = "#00ffff00",
-								shape  = gears.shape.circle,
-								border_width = 2,
-								border_color = beautiful.fg_normal,
-								widget = wibox.container.background,
-							},
-							layout = wibox.layout.fixed.horizontal,
-						},
-						left  = 8,
-						right = 8,
-						top   = 8,
-						bottom = 8,
-						widget = wibox.container.margin,
-					},
-					--id     = "background_role",
-					widget = wibox.container.background,
-					bg = "ffff00",
-					create_callback = function(self, c3, index, objects) --luacheck: no unused args
-						self:get_children_by_id("index_role")[1].markup = "<b> "..c3.index.." </b>"
-						local circle = self.children[1].children[1].children[1]
-						self:connect_signal("mouse::enter", function()
-							if circle.bg ~= "#00000000" then
-								circle.backup     = circle.bg
-								circle.has_backup = true
-							end
-							circle.bg = "#00ff00"
-						end)
-						self:connect_signal("mouse::leave", function()
-							if circle.has_backup then circle.bg = circle.backup end
-						end)
-						if c3.selected then
-							circle.bg = beautiful.bg_focus
-						end
-						if c3.selected == false then
-							circle.bg = "#00000000"
-						end
-					end,
-					update_callback = function(self, c3, index, objects) --luacheck: no unused args
-						self:get_children_by_id("index_role")[1].markup = "<b> "..c3.index.." </b>"
+  -- Create a promptbox for each screen
+  s.mypromptbox = awful.widget.prompt()
+  -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+  -- We need one layoutbox per screen.
+  s.mylayoutbox = awful.widget.layoutbox(s)
+  s.mylayoutbox = wibox.container.margin(
+    s.mylayoutbox,
+    6, 6, 6, 6
+  )
+  s.mylayoutbox:buttons(gears.table.join(
+    awful.button({}, 1, function() awful.layout.inc(1) end),
+    awful.button({}, 3, function() awful.layout.inc(-1) end),
+    awful.button({}, 4, function() awful.layout.inc(1) end),
+    awful.button({}, 5, function() awful.layout.inc(-1) end)))
+  -- Create a taglist widget
 
-						local circle = self.children[1].children[1].children[1]
-						if c3.selected then
-							circle.bg = beautiful.bg_focus
-						end
-						if c3.selected == false then
-							circle.bg = "#00000000"
-						end
-					end,
-			},
-	}
-    -- Create a tasklist widget
-  	s.mytasklist = awful.widget.tasklist {
-       screen  = s,
-       filter  = awful.widget.tasklist.filter.currenttags,
-       buttons = tasklist_buttons
-    }
-
-    -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s, height = 44 })
-
-	local system_menu = require("widgets/system_menu")
-	
-	local rw = wibox.widget {
-		bg = "#00000000",
-		widget = wibox.container.background 
-	}	
-	original_bg = rw.bg
-	rw:connect_signal("mouse::enter", function() 
-										--rw.bg = beautiful.bg_normal
-										rw.bg = "#00000065" 
-									  	end)
-	rw:connect_signal("mouse::leave", function() rw.bg = original_bg end)
-	rw:connect_signal("button::press", function() system_menu.toggleMenu()  end)	
-	--system_menu.button:connect_signal("button::press", function() system_menu.toggleMenu() end)
-	local widgets = {
-		{			
-			--tray.button,
-  			wibox.widget.textbox('    '),
-			wifi.icon,
-            wibox.widget.textbox('    '),
-			battery.percent,
-			battery.icon,
-			wibox.widget.textbox('    '),
-			audio.level,
-			audio.icon,
-			wibox.widget.textbox('    '),
-			layout = wibox.layout.fixed.horizontal,
-		},
-		widget = rw
-	}
-
-	local r_widgets = {
-		tray.systray,	
-		wibox.widget.textbox('    '),
-		s.mylayoutbox,
-		widgets,
-		system_menu.button,
-		layout = wibox.layout.fixed.horizontal,
-	
-	}
-
-	--r_widgets:connect_signal("mouse::leave", function() bgSwap() end)
-	
-    -- Add widgets to the wibox
-    s.mywibox:setup {
-		expand = "none",
-        layout = wibox.layout.align.horizontal,
-        { -- Left widgets
-            layout = wibox.layout.fixed.horizontal,
-			--spacing = 20,
-            launcher.mylauncher,
-            s.mytaglist,
-            s.mypromptbox,
-			--s.mytasklist, 
+  s.mytaglist = awful.widget.taglist {
+    screen          = s,
+    filter          = awful.widget.taglist.filter.all,
+    layout          = {
+      spacing = 0,
+      layout  = wibox.layout.fixed.horizontal
+    },
+    buttons         = {
+      awful.button({}, 1, function(t) t:view_only() end),
+      awful.button({ modkey }, 1, function(t)
+        if client.focus then
+          client.focus:move_to_tag(t)
+        end
+      end),
+      awful.button({}, 3, awful.tag.viewtoggle),
+      awful.button({ modkey }, 3, function(t)
+        if client.focus then
+          client.focus:toggle_tag(t)
+        end
+      end),
+      awful.button({}, 4, function(t) awful.tag.viewprev(t.screen) end),
+      awful.button({}, 5, function(t) awful.tag.viewnext(t.screen) end),
+    },
+    widget_template = {
+      {
+        {
+          {
+            {
+              id      = "index_role",
+              opacity = 0,
+              widget  = wibox.widget.textbox,
+            },
+            bg           = "#00ffff00",
+            shape        = gears.shape.circle,
+            border_width = 2,
+            border_color = beautiful.fg_normal,
+            widget       = wibox.container.background,
+          },
+          layout = wibox.layout.fixed.horizontal,
         },
-		clock.textclock,
-  		r_widgets	
-    }
+        left   = 8,
+        right  = 8,
+        top    = 8,
+        bottom = 8,
+        widget = wibox.container.margin,
+      },
+      --id     = "background_role",
+      widget = wibox.container.background,
+      bg = "ffff00",
+      create_callback = function(self, c3, index, objects) --luacheck: no unused args
+        self:get_children_by_id("index_role")[1].markup = "<b> " .. c3.index .. " </b>"
+        local circle = self.children[1].children[1].children[1]
+        self:connect_signal("mouse::enter", function()
+          if circle.bg ~= "#00000000" then
+            circle.backup     = circle.bg
+            circle.has_backup = true
+          end
+          circle.bg = "#00ff00"
+        end)
+        self:connect_signal("mouse::leave", function()
+          if circle.has_backup then circle.bg = circle.backup end
+        end)
+        if c3.selected then
+          circle.bg = beautiful.bg_focus
+        end
+        if c3.selected == false then
+          circle.bg = "#00000000"
+        end
+--[[
+		self:connect_signal("button::press", function()
+			circle.bg = beautiful.bg_focus
+		end)
+]]--
+      end,
+      update_callback = function(self, c3, index, objects) --luacheck: no unused args
+        self:get_children_by_id("index_role")[1].markup = "<b> " .. c3.index .. " </b>"
+
+        local circle = self.children[1].children[1].children[1]
+        if c3.selected then
+          circle.bg = beautiful.bg_focus
+        end
+        if c3.selected == false then
+          circle.bg = "#00000000"
+        end
+--[[
+		self:connect_signal("button::press", function()
+			circle.bg = beautiful.bg_focus
+		end)
+]]--
+      end,
+    },
+  }
+  -- Create a tasklist widget
+  s.mytasklist = awful.widget.tasklist {
+    screen  = s,
+    filter  = awful.widget.tasklist.filter.currenttags,
+    buttons = tasklist_buttons
+  }
+
+  -- Create the wibox
+  s.mywibox = awful.wibar({ position = "top", screen = s, height = 44 })
+
+  local system_menu = require("widgets/system_menu")
+
+  local rw = wibox.widget {
+    bg = "#00000000",
+    widget = wibox.container.background
+  }
+  original_bg = rw.bg
+  rw:connect_signal("mouse::enter", function()
+    --rw.bg = beautiful.bg_normal
+    rw.bg = "#00000065"
+  end)
+  rw:connect_signal("mouse::leave", function() rw.bg = original_bg end)
+  rw:connect_signal("button::press", function() system_menu.toggleMenu() end)
+  --system_menu.button:connect_signal("button::press", function() system_menu.toggleMenu() end)
+  local widgets = {
+    {
+      --tray.button,
+      wibox.widget.textbox('    '),
+      wifi.icon,
+      wibox.widget.textbox('    '),
+      battery.percent,
+      battery.icon,
+      wibox.widget.textbox('    '),
+      audio.level,
+      audio.icon,
+      wibox.widget.textbox('    '),
+      layout = wibox.layout.fixed.horizontal,
+    },
+    widget = rw
+  }
+
+  local r_widgets = {
+    tray.systray,
+    wibox.widget.textbox('    '),
+    s.mylayoutbox,
+    widgets,
+    system_menu.button,
+    layout = wibox.layout.fixed.horizontal,
+
+  }
+
+  --r_widgets:connect_signal("mouse::leave", function() bgSwap() end)
+
+  -- Add widgets to the wibox
+  s.mywibox:setup {
+    expand = "none",
+    layout = wibox.layout.align.horizontal,
+    { -- Left widgets
+      layout = wibox.layout.fixed.horizontal,
+      --spacing = 20,
+      launcher.mylauncher,
+      s.mytaglist,
+      s.mypromptbox,
+      --s.mytasklist,
+    },
+    clock.textclock,
+    r_widgets
+  }
 end)
--- .config/awesome/widgets/wifi.lua
+++ .config/awesome/widgets/wifi.lua
@@ -15,70 +15,71 @@ local b_icon = wibox.widget.imagebox()
 b_icon:set_image(beautiful.bars)
 local button_margin = wibox.container.margin(b_icon, 20, 20, 20, 20)
 local button_bg = wibox.container {
-	button_margin, 
-	bg = "#00000000",
-	shape        = gears.shape.rounded_rect,
-	widget = wibox.container.background,
+  button_margin,
+  bg     = "#00000000",
+  shape  = gears.shape.rounded_rect,
+  widget = wibox.container.background,
 }
 
 button_bg:connect_signal("mouse::enter", function() button:hoverOn(button_bg) end)
 button_bg:connect_signal("mouse::leave", function() button:hoverOff(button_bg) end)
 
 wifi.button = wibox.container {
-	button_bg,
-	valign = center,
-	halign = center,
-	widget = wibox.container.place,
+  button_bg,
+  valign = center,
+  halign = center,
+  widget = wibox.container.place,
 }
 
 cmd = "cat /proc/net/wireless | sed -n '3 p' | cut -d ' ' -f 6"
 
 -- derived from https://awesomewm.org/recipes/wirelessStatus/
-wifi.status = awful.widget.watch('bash -c "cat /proc/net/wireless | sed -n \'3 p\' | cut -d \' \' -f 6 | head -c -2"', 1,
-		function(widget, stdout)
-			if (stdout == "") 
-			then
-				wifi_image:set_image(beautiful.wifi_ds)
-				b_icon:set_image(beautiful.wifi_ds)
-			else	
-				quality = stdout * (10 / 7)
+wifi.status = awful.widget.watch('bash -c "cat /proc/net/wireless | sed -n \'3 p\' | cut -d \' \' -f 6 | head -c -2"', 1
+  ,
+  function(widget, stdout)
+	if (stdout == "")
+    then
+      wifi_image:set_image(beautiful.wifi_dc)
+      b_icon:set_image(beautiful.wifi_dc)
+    else
+      quality = stdout * (10 / 7)
 
-				if (quality <= 25) then
-					wifi_image:set_image(beautiful.wifi_0)
-					b_icon:set_image(beautiful.wifi_0)
-				end
-				if (quality <= 50) then
-					wifi_image:set_image(beautiful.wifi_1)
-					b_icon:set_image(beautiful.wifi_1)
-				end
-				if (quality <= 75) then
-					wifi_image:set_image(beautiful.wifi_2)
-					b_icon:set_image(beautiful.wifi_2)
-				end
-				if (quality > 75) then
-					wifi_image:set_image(beautiful.wifi_3)
-					b_icon:set_image(beautiful.wifi_3)
-				end
-				widget:set_text(quality)
-		end
-	end
+      if (quality <= 25) then
+        wifi_image:set_image(beautiful.wifi_0)
+        b_icon:set_image(beautiful.wifi_0)
+      end
+      if (quality <= 50) then
+        wifi_image:set_image(beautiful.wifi_1)
+        b_icon:set_image(beautiful.wifi_1)
+      end
+      if (quality <= 75) then
+        wifi_image:set_image(beautiful.wifi_2)
+        b_icon:set_image(beautiful.wifi_2)
+      end
+      if (quality > 75) then
+        wifi_image:set_image(beautiful.wifi_3)
+        b_icon:set_image(beautiful.wifi_3)
+      end
+      widget:set_text(quality)
+    end
+  end
 )
 
 
 -- iw dev
 
 wifi.icon = wibox.container.margin(
-	wifi_image, 
-	3, 3, 3, 3
+  wifi_image,
+  3, 3, 3, 3
 )
 
 
 wifi.ssid = wibox.widget {
-	widget = wibox.widget.textbox,
+  widget = wibox.widget.textbox,
 }
 
-awful.spawn.easy_async_with_shell("iwgetid -r", function(stdout) 
-	wifi.ssid.text = stdout 
+awful.spawn.easy_async_with_shell("iwgetid -r", function(stdout)
+  wifi.ssid.text = stdout
 end)
 
 
